0cbca593a7a419d734d6b6e4670578e5445eb7e0
Ryan Caezar Itang <sitiom@disroot.org>
2022-07-12 11:00:45 +0800

    Merge with nvim-numbertoggle

--- lua/numbers/init.lua
+++ lua/numbers/init.lua
@@ -1,60 +1,65 @@
 local function autocmd(events, command)
-  vim.api.nvim_create_autocmd(events, {group='NumbersAutocmds', command=command})
+   vim.api.nvim_create_autocmd(events, { group = "NumbersAutocmds", callback = command })
 end
 
 M = {}
 
 M.options = {
-  excluded_filetypes = {
-    unite = true,
-    tagbar = true,
-    startify = true,
-    gundo = true,
-    vimshell = true,
-    w3m = true,
-    nerdtree = true,
-    Mundo = true,
-    MundoDiff = true,
-  },
-  norelative_events = {
-    'InsertEnter',
-    'WinLeave',
-    'FocusLost',
-    'BufNewFile',
-    'BufReadPost',
-  },
-  relative_events = {
-    'InsertLeave',
-    'WinEnter',
-    'FocusGained',
-  },
+   excluded_filetypes = {
+      unite = true,
+      tagbar = true,
+      startify = true,
+      gundo = true,
+      vimshell = true,
+      w3m = true,
+      nerdtree = true,
+      Mundo = true,
+      MundoDiff = true,
+   },
+   norelative_events = {
+      "BufLeave",
+      "FocusLost",
+      "InsertEnter",
+      "CmdlineEnter",
+      "WinLeave",
+   },
+   relative_events = {
+      "BufEnter",
+      "FocusGained",
+      "InsertLeave",
+      "CmdlineLeave",
+      "WinEnter",
+   },
 }
 
-
-M.set_numbering = function(relative)
-  if M.options.excluded_filetypes[vim.api.nvim_buf_get_option(0, 'filetype')] then
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-    return
-  end
-  vim.opt.number = true
-  vim.opt.relativenumber = relative
+function M.set_numbering(relative)
+   if M.options.excluded_filetypes[vim.api.nvim_buf_get_option(0, "filetype")] then
+      vim.o.number = false
+      vim.o.relativenumber = false
+   elseif vim.o.nu then
+      vim.o.relativenumber = relative and vim.api.nvim_get_mode().mode ~= "i"
+      vim.cmd "redraw"
+   end
 end
 
-M.setup = function(options)
-  options = options or {}
-  if options.excluded_filetypes then
-    M.options.excluded_filetypes = {}
+function M.setup(options)
+   options = options or {}
+   if options.excluded_filetypes then
+      M.options.excluded_filetypes = {}
 
-    for _, v in pairs(options.excluded_filetypes) do
-      M.options.excluded_filetypes[v] = true
-    end
-  end
+      for _, v in pairs(options.excluded_filetypes) do
+         M.options.excluded_filetypes[v] = true
+      end
+   end
 
-  vim.api.nvim_create_augroup('NumbersAutocmds', {clear=true})
+   vim.api.nvim_create_augroup("NumbersAutocmds", { clear = true })
 
-  autocmd(M.options.norelative_events, 'lua require("numbers").set_numbering(false)')
-  autocmd(M.options.relative_events, 'lua require("numbers").set_numbering(true)')
+   autocmd(M.options.norelative_events, function()
+      M.set_numbering(false)
+   end)
+   autocmd(M.options.relative_events, function()
+      M.set_numbering(true)
+   end)
 end
 
 return M
